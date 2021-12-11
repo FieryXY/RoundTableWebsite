eba520db26e6e5d5591aa613fdfc00d77ca4cf4a
sohanagarkar <33811100+FieryXY@users.noreply.github.com>
2021-12-11 15:40:48 -0800

    Merged Riley's Homepage code

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,7 +1,9 @@
 function riseMenu() {
-  document.getElementById("homeNavBar").style.width = "25rem";
+  document.getElementById("homeNavBar").style.width = "20%";
+  document.getElementById("centralBox").style.marginLeft = "30%";
 }
 
 function closeMenu() {
   document.getElementById("homeNavBar").style.width = "0%";
+  document.getElementById("centralBox").style.marginLeft = "10%";
 }
