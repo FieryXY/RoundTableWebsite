413cae88feea65fc170278bc210450e6e138d369
sohanagarkar <33811100+FieryXY@users.noreply.github.com>
2021-12-11 16:02:03 -0800

    Made Separate Divs for each text box, made Nav Bar text scalable to screen size

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,11 @@
+const centralBoxes = document.getElementsByClassName("column");
+
 function riseMenu() {
   document.getElementById("homeNavBar").style.width = "20%";
-  document.getElementById("centralBox").style.marginLeft = "30%";
+  document.getElementById("siteContent").style.marginLeft = "20%";
 }
 
 function closeMenu() {
   document.getElementById("homeNavBar").style.width = "0%";
-  document.getElementById("centralBox").style.marginLeft = "10%";
+  document.getElementById("siteContent").style.marginLeft = "0%";
 }
