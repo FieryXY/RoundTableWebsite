f6312132ef54e49df4564d4e528edecae5a8b9d2
SirEdward <36611325+Sir-Edward-123@users.noreply.github.com>
2021-12-12 06:03:01 +0000

    I changed some code organization, function names, and stuff like that.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,13 +1,15 @@
 const centralBoxes = document.getElementsByClassName("column");
-
+ 
 function riseMenu() {
   document.getElementById("homeNavBar").classList.remove("closed");
   document.getElementById("homeNavBar").classList.add("open");
   document.getElementById("siteContent").classList.add("adjusted");
+  document.getElementById("header").classList.add("adjusted");
 }
-
+ 
 function closeMenu() {
   document.getElementById("homeNavBar").classList.remove("open");
   document.getElementById("homeNavBar").classList.add("closed");
   document.getElementById("siteContent").classList.remove("adjusted");
+  document.getElementById("header").classList.remove("adjusted");
 }
